8d45db71c18f55ae124f270e01ac85e14895ec23
Fernando <hidabe@gmail.com>
2016-08-11 16:03:43 +0000

    Creado nuevo fichero controlador para la ruta home

---
+++ src/AppBundle/Controller/PublicController.php
@@ -0,0 +1,17 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PublicController extends Controller
+{
+    /**
+     * @Route("/", name="public_home")
+     */
+    public function homeAction(Request $request)
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
