20ad8b4210f15427fe20ca7931ec889809776d91
Fernando <hidabe@gmail.com>
2016-08-18 10:21:32 +0000

    Integración de FOSUserBundle con EasyAdmin

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+namespace AppBundle\Controller;
+
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+
+class AdminController extends BaseAdminController
+{
+    public function createNewPersonaEntity()
+    {
+        return $this->get('fos_user.user_manager')->createUser();
+    }
+
+    public function prePersistPersonaEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+    }
+    
+    public function preUpdatePersonaEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+    }
+}
