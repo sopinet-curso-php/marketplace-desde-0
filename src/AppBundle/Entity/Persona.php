beb7e4e095a6a098d1a4cb5ccf7e2491e31a9ed8
Fernando <hidabe@gmail.com>
2016-08-18 10:56:05 +0000

    14. Modificación de entidad Persona

--- src/AppBundle/Entity/Persona.php
+++ src/AppBundle/Entity/Persona.php
@@ -16,10 +16,78 @@ class Persona extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $avatar;    
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Trayecto", mappedBy="conductor")
+     */
+    protected $trayectos;    
 
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Set avatar
+     *
+     * @param string $avatar
+     *
+     * @return Persona
+     */
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    /**
+     * Get avatar
+     *
+     * @return string
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * Add trayecto
+     *
+     * @param \AppBundle\Entity\Trayecto $trayecto
+     *
+     * @return Persona
+     */
+    public function addTrayecto(\AppBundle\Entity\Trayecto $trayecto)
+    {
+        $this->trayectos[] = $trayecto;
+
+        return $this;
+    }
+
+    /**
+     * Remove trayecto
+     *
+     * @param \AppBundle\Entity\Trayecto $trayecto
+     */
+    public function removeTrayecto(\AppBundle\Entity\Trayecto $trayecto)
+    {
+        $this->trayectos->removeElement($trayecto);
+    }
+
+    /**
+     * Get trayectos
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getTrayectos()
+    {
+        return $this->trayectos;
     }
 }
